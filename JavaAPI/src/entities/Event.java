8cdb3ac4995f820e8bc4d5dafcf782bdd9a2650b
Tapaswi <tapaswisatpanthi@gmail.com>
2019-06-18 09:56:32 +0530

    18-June-2019 9:56 AM by tapaswi

---
+++ JavaAPI/src/entities/Event.java
@@ -0,0 +1,95 @@
+package entities;
+
+public class Event {
+
+	int id;
+	String date,grp,startTime,endTime,activity,description,venue;
+	
+	public Event() {
+		
+	}
+	
+	public Event(String date, String grp, String startTime, String endTime, String activity, String venue) {
+		super();
+		this.date = date;
+		this.grp = grp;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.activity = activity;
+		this.venue = venue;
+	}
+
+	public Event(String date, String grp, String startTime, String endTime, String activity, String description,
+			String venue) {
+		super();
+		this.date = date;
+		this.grp = grp;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.activity = activity;
+		this.description = description;
+		this.venue = venue;
+	}
+	public Event(int id, String date, String grp, String startTime, String endTime, String activity, String description,
+			String venue) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.grp = grp;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.activity = activity;
+		this.description = description;
+		this.venue = venue;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getGrp() {
+		return grp;
+	}
+	public void setGrp(String grp) {
+		this.grp = grp;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	public String getActivity() {
+		return activity;
+	}
+	public void setActivity(String activity) {
+		this.activity = activity;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getVenue() {
+		return venue;
+	}
+	public void setVenue(String venue) {
+		this.venue = venue;
+	}
+	
+	
+}
--- JavaAPI/src/services/Services.java
+++ JavaAPI/src/services/Services.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -17,6 +18,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import entities.User;
+import entities.Event;
 
 @Path("/services")
 public class Services {
@@ -28,17 +30,11 @@ public class Services {
 
 	void doConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
-		con = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/kgJukQ0geQ", "kgJukQ0geQ", "haebUrLDc8");
+		con = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/kgJukQ0geQ?useSSL=false", "kgJukQ0geQ", "haebUrLDc8");
 	}
 
 	public Services() {
 		// TODO Auto-generated constructor stub
-		try {
-			doConnection();
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 	@GET
@@ -47,6 +43,7 @@ public class Services {
 	public List<User> getAllRecords() {
 		List<User> li = new ArrayList<User>();
 		try {
+			doConnection();
 			st = con.createStatement();
 			rs = st.executeQuery("SELECT * FROM tbl_student_details");
 			while (rs.next()) {
@@ -54,9 +51,15 @@ public class Services {
 			}
 			rs.close();
 			st.close();
-
+			con.close();
 		} catch (Exception e) {
 			e.printStackTrace();
+			try {
+				con.close();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		}
 		return li;
 
@@ -68,6 +71,7 @@ public class Services {
 	@Consumes("application/json")
 	public boolean authenticate(User user) {
 		try {
+			doConnection();
 			if (user.getType().equals("Student")) {
 				ps = con.prepareStatement("SELECT * FROM tbl_student_details WHERE Email_Id = ? AND Password = ?");
 				ps.setString(1, user.getEmailId());
@@ -75,28 +79,42 @@ public class Services {
 				rs = ps.executeQuery();
 				while (rs.next()) {
 					if (rs.getString("Email_Id").equals(user.getEmailId())
-							&& rs.getString("Password").equals(user.getPassword()))
+							&& rs.getString("Password").equals(user.getPassword())) {
+						con.close();
 						return true;
-					else
+					} else {
+						con.close();
 						return false;
+					}
 				}
 			} else if (user.getType().equals("Admin")) {
 				ps = con.prepareStatement("SELECT * FROM tbl_admin_details WHERE Email_Id = ? AND Password = ?");
 				ps.setString(1, user.getEmailId());
 				ps.setString(2, user.getPassword());
+
 				rs = ps.executeQuery();
 				while (rs.next()) {
 					if (rs.getString("Email_Id").equals(user.getEmailId())
-							&& rs.getString("Password").equals(user.getPassword()))
+							&& rs.getString("Password").equals(user.getPassword())) {
+						con.close();
 						return true;
-					else
+					} else {
+						con.close();
 						return false;
+					}
 				}
 			}
+			con.close();
 			return false;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			try {
+				con.close();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			return false;
 		}
 	}
@@ -107,16 +125,29 @@ public class Services {
 	@Consumes("application/json")
 	public String register(User user) {
 		if (checkIfAlreadyRegistered(user.getEmailId())) {
+			try {
+				con.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			return "Already Registered";
 		} else {
 			try {
 				ps = con.prepareStatement("INSERT INTO tbl_student_details(Email_Id,Password) values(?,?)");
-				ps.setString(1,user.getEmailId());
-				ps.setString(2,user.getPassword());
+				ps.setString(1, user.getEmailId());
+				ps.setString(2, user.getPassword());
 				ps.execute();
+				con.close();
 				return "Done";
-			}catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
+				try {
+					con.close();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				return "Error";
 			}
 		}
@@ -124,6 +155,7 @@ public class Services {
 
 	private boolean checkIfAlreadyRegistered(String emailId) {
 		try {
+			doConnection();
 			ps = con.prepareStatement("SELECT Email_Id FROM tbl_student_details WHERE Email_Id = ?");
 			ps.setString(1, emailId);
 			return ps.executeQuery().first();
@@ -133,7 +165,186 @@ public class Services {
 		return false;
 	}
 
+	@POST
+	@Path("/recovery")
+	@Produces("application/json")
+	@Consumes("application/json")
+	public String passwordRecovery(User user) {
+		try {
+			if (checkIfAlreadyRegistered(user.getEmailId())) {
+				ps = con.prepareStatement("UPDATE tbl_student_details SET Password = ? WHERE Email_Id = ?");
+				ps.setString(1, user.getPassword());
+				ps.setString(2, user.getEmailId());
+				if (ps.executeUpdate() > 0) {
+					con.close();
+					return "Done";
+				} else {
+					con.close();
+					return "Error";
+				}
+			} else {
+				con.close();
+				return "Not Registered";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				con.close();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			return "Error";
+		}
+	}
+
+	@GET
+	@Path("/getAllEvent")
+	@Produces("application/json")
+	@Consumes("application/json")
+	public List<Event> getAllEvent() {
+		List<Event> events = new ArrayList<Event>();
+		try {
+			doConnection();
+			ps = con.prepareStatement("SELECT * FROM tbl_event_details");
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				Event event = new Event(rs.getInt("ID"), rs.getString("Date"), rs.getString("Grp"),
+						rs.getString("Start_Time"), rs.getString("End_Time"), rs.getString("Activity"),
+						rs.getString("Description"), rs.getString("Venue"));
+				events.add(event);
+				event = null;
+			}
+			con.close();
+			return events;
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				con.close();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			return null;
+		}
+	}
+
 	
-	 @Override protected void finalize() throws Throwable { super.finalize(); con.close(); }
-	 
+	@GET
+	@Path("/getAllEventDateWise")
+	@Produces("application/json")
+	@Consumes("application/json")
+	public List<Event> getAllEventDateWise(Event event) {
+		List<Event> events = new ArrayList<Event>();
+		try {
+			doConnection();
+			ps = con.prepareStatement("SELECT * FROM tbl_event_details WHERE Date = ?");
+			ps.setString(1,event.getDate());
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				Event e = new Event(rs.getInt("ID"), rs.getString("Date"), rs.getString("Grp"),
+						rs.getString("Start_Time"), rs.getString("End_Time"), rs.getString("Activity"),
+						rs.getString("Description"), rs.getString("Venue"));
+				events.add(e);
+				e = null;
+			}
+			con.close();
+			return events;
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				con.close();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			return null;
+		}
+	}
+	
+	@POST
+	@Path("/addEvent")
+	@Produces("application/json")
+	@Consumes("application/json")
+	public String addEvent(Event event) {
+		try {
+			doConnection();
+			ps = con.prepareStatement(
+					"INSERT INTO tbl_event_details(Date, Grp, Start_Time, End_Time, Activity, Description, Venue) VALUES"
+							+ "(?,?,?,?,?,?,?)");
+			ps.setString(1, event.getDate());
+			ps.setString(2, event.getGrp());
+			ps.setString(3, event.getStartTime());
+			ps.setString(4, event.getEndTime());
+			ps.setString(5, event.getActivity());
+			ps.setString(6, event.getDescription());
+			ps.setString(7, event.getVenue());
+			ps.execute();
+			con.close();
+			return "Done";
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				con.close();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			return "Error";
+		}
+	}
+
+	@DELETE
+	@Path("/deleteEvent/{id}")
+	@Produces("application/json")
+	public String deleteEvent(@PathParam("id") int id) {
+		try {
+			doConnection();
+			ps = con.prepareStatement("DELETE FROM tbl_event_details WHERE ID = ?");
+			ps.setInt(1, id);
+			ps.execute();
+			return "Done";
+		}catch(Exception e) {
+			e.printStackTrace();
+			try {
+				con.close();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			return "Error";
+		}
+	}
+	
+	@POST
+	@Path("/updateEvent")
+	@Produces("application/json")
+	public String updateEvent(Event event) {
+		try {
+			doConnection();
+			ps = con.prepareStatement("UPDATE tbl_event_details SET Date = ?, Grp = ?, Start_Time = ?, End_Time = ?, "
+					+ "Activity = ?, Description = ?, Venue = ? WHERE ID = ?");
+			ps.setString(1, event.getDate());
+			ps.setString(2, event.getGrp());
+			ps.setString(3, event.getStartTime());
+			ps.setString(4, event.getEndTime());
+			ps.setString(5, event.getActivity());
+			ps.setString(6, event.getDescription());
+			ps.setString(7, event.getVenue());
+			ps.setInt(8, event.getId());
+			ps.execute();
+			ps.close();
+			con.close();
+			return "Done";
+		}catch(Exception e) {
+			try {
+				con.close();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+			return "Error";
+		}
+	}
 }
